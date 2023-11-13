738f345a702568ceb1a05704edc03a754c00d2ba
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-13 17:28:06 +0100

    Aggiungere i file di progetto.

---
+++ Pick'nMix/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+
+Program program = new Program();
+
+ArrayList arrayList = new ArrayList();
+
+
+arrayList.Add(1);
+arrayList.Add("due");
+arrayList.Add(program);
+arrayList.Add(true);
+arrayList.Add(false);
+
+
+Console.WriteLine("Contenuto dell'ArrayList:");
+
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine(arrayList[i]);
+}
+
+/*gli elementi vengono stampati senza un tipo specifico, 
+ * mostrando semplicemente il loro valore.
+ * Questo accade perchè nell'ArrayList tutti i dati inseriti vengono trattati come tipo object
+ */
